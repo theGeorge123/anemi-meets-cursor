a58328170f551f416ac6eb04a9a206de726dc4cf
theGeorge123 <maxmeinders2002@gmail.com>
2025-06-04 21:26:16 +0200

    fix: unify supabase initialization

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,8 +1,1 @@
-import { createClient } from '@supabase/supabase-js';
-
-// console.log('DEBUG SUPABASE_URL:', import.meta.env.VITE_SUPABASE_URL);
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey); 
+export { supabase } from './lib/supabase';
